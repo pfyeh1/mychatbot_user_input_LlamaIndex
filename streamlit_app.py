1c8e56280a609c74ee6d9bce956603b9ee2f7f1d
pfyeh1 <pfyeh1@gmail.com>
2024-01-15 19:39:43 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,15 @@
 import streamlit as st
+from llama_index import VectorStoreIndex, ServiceContext, Document
+from llama_index.llms import OpenAI
+import openai
+from llama_index import SimpleDirectoryReader
+
 
 st.title('🎈 App Name')
 
 st.write('Hello world!')
+
+uploaded_file = st.file_uploader('Choose your .pdf file', type="pdf")
+
+if uploaded_file:
+    st.write("FIlename: ", uploaded_file.name)
