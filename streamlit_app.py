29d8dbbe7b9fc46378f16374dc57bee565e5cb99
pfyeh1 <pfyeh1@gmail.com>
2024-01-15 21:50:55 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from llama_index import VectorStoreIndex, ServiceContext, Document
 from llama_index.llms import OpenAI
 import openai
-#from llama_index import PDFReader
+from llama_index import download_loader
 
 st.title('🎈 huh?')
 
@@ -10,3 +10,8 @@ openai.api_key = st.secrets.openai_key
 st.header("Chat with your uploaded file 💬 📚")
 
 pdf_file = st.file_uploader('Choose your .pdf file', type="pdf")
+
+PDFReader = download_loader("PDFReader")
+
+loader = PDFReader()
+data = loader.load_data(pdf_file)
