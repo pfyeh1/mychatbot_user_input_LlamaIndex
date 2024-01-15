36a6a2ccf039d980e56c3311ed48371ff8329a28
pfyeh1 <pfyeh1@gmail.com>
2024-01-15 21:46:55 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,12 @@ import streamlit as st
 from llama_index import VectorStoreIndex, ServiceContext, Document
 from llama_index.llms import OpenAI
 import openai
-from llama_index import PDFReader
+#from llama_index import PDFReader
 
 st.title('🎈 huh?')
 
 openai.api_key = st.secrets.openai_key
 st.header("Chat with your uploaded file 💬 📚")
 
-#@st.cache_resource(show_spinner = False)
+@st.cache_resource(show_spinner = False)
 pdf_file = st.file_uploader('Choose your .pdf file', type="pdf")
