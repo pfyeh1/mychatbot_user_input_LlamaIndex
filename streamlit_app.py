c8d0cd63d66bcefb160f91772aa132b85418094d
pfyeh1 <pfyeh1@gmail.com>
2024-01-15 20:20:18 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,17 @@ from llama_index.llms import OpenAI
 import openai
 from llama_index import SimpleDirectoryReader
 
+st.title('🎈 huh?')
 
-st.title('🎈 App Name')
+openai.api_key = st.secrets.openai_key
+st.header("Chat with your uploaded file 💬 📚")
 
-st.write('Hello world!')
+if "messages" not in st.session_state.keys(): # Initialize the chat message history
+    st.session_state.messages = [
+        {"role": "assistant", "content": "Ask me a question!"}
+    ]
+    
+@st.cache_resource(show_spinner = False)
 
 uploaded_file = st.file_uploader('Choose your .pdf file', type="pdf")
 
