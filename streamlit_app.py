f7ff3956fefc5a327e564a5718292e91ab4c7c38
pfyeh1 <pfyeh1@gmail.com>
2024-01-15 21:48:02 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,5 +9,4 @@ st.title('🎈 huh?')
 openai.api_key = st.secrets.openai_key
 st.header("Chat with your uploaded file 💬 📚")
 
-@st.cache_resource(show_spinner = False)
 pdf_file = st.file_uploader('Choose your .pdf file', type="pdf")
