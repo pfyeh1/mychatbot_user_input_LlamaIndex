182938c21c647170135c60a20b57e05dc68aecb3
pfyeh1 <pfyeh1@gmail.com>
2024-01-16 01:14:25 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from llama_index import VectorStoreIndex, ServiceContext, Document
 from llama_index.llms import OpenAI
 import openai
-from llama_index import download_loader
+from pypdf import PdfReader
 
 st.title('🎈 huh?')
 
@@ -18,35 +18,31 @@ def load_data():
                                                             model = "gpt-3.5-turbo",
                                                             temperature = .5,
                                                             system_prompt = 
-                                                           "Your job is to answer questions on the relate to the documents. Keep your answers technical, based on facts, and explain your reasoning."))
+                                                           "Your job is to answer questions on the relate to the documents. Keep your                                                                     answers technical, based on facts, and explain your reasoning."))
         index = VectorStoreIndex.from_documents(reader, service_context = service_context)
         return index
         
-pdf_file = st.file_uploader('Choose your .pdf file', type="pdf")
+        
+def get_pdf_text(pdf_file):
+    text = ""
+    pdf_reader = PdfReader(pdf_file)
+    for page in pdf_reader.pages:
+        text += page.extract_text()
+    return text
 
+def process_text(text):
+    doc = Document(text = text)
+    return [doc]
+
+
+pdf_file = st.file_uploader('Choose your .pdf file', type="pdf")
 
 if pdf_file is not None:
     st.write("Uploaded Filename: ", pdf_file.name)
-
-    # load data
-    index = load_data()
     
-    chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
+    text = get_pdf_text(pdf_file)
+    st.write("PDF content extracted!")
+    st.write(text)
+    
 
-    if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
-        st.session_state.messages.append({"role": "user", "content": prompt})
-
-    for message in st.session_state.messages: # Display the prior chat messages
-        with st.chat_message(message["role"]):
-            st.write(message["content"])
-        
-        
-    # If last message is not from assistant, generate a new response
-    if st.session_state.messages[-1]["role"] != "assistant":
-        with st.chat_message("assistant"):
-            with st.spinner("Tinkin..."):
-                response = chat_engine.chat(prompt)
-                st.write(response.response)
-                message = {"role": "assistant", "content": response.response}
-                st.session_state.messages.append(message) # Add response to message history
-                
+   
